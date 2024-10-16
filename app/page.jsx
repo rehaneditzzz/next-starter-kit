07fbd6797e14cf585640d42c613dd91f438a155c
Yash <155367011+rehaneditzzz@users.noreply.github.com>
2024-10-16 20:38:30 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css"
+
+export default function Home() {
+  return (
+   <section className="py-24">
+    <div className="container">
+      <h1 className={styles.title}>Next js starter kit</h1>
+    </div>
+   </section>
+  );
+}
